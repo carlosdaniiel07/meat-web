0451a9c7499e742f1be10520bf9653e5fd5fd3c1
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-07-10 15:52:36 -0300

    Add loading to restaurant details page and hide shopping cart

--- src/app/pages/restaurants/restaurant-details/restaurant-details.component.ts
+++ src/app/pages/restaurants/restaurant-details/restaurant-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations'
+import { Observable } from 'rxjs'
 import { Restaurant } from 'src/app/models'
+import { LoadingService } from 'src/app/services/loading.service'
 import { RestaurantService } from 'src/app/services/restaurant.service'
 
 @Component({
@@ -18,12 +20,17 @@ export class RestaurantDetailsComponent implements OnInit {
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly restaurantService: RestaurantService
+    private readonly restaurantService: RestaurantService,
+    private readonly loadingService: LoadingService,
   ) { }
 
   ngOnInit(): void {
     const restaurantId = this.route.snapshot.params.id
     this.loadRestaurant(restaurantId)
+  }
+
+  public get loading(): Observable<boolean> {
+    return this.loadingService.loading$
   }
 
   private loadRestaurant(id: string): void {
