0e88484723a49166478d5b60df73355844fddf5f
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-07-10 15:47:17 -0300

    Update restaurant.service.ts

--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { environment } from '../../environments/environment';
-import { Restaurant } from '../models';
+import { Observable } from 'rxjs'
+import { environment } from '../../environments/environment'
+import { Restaurant } from '../models'
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class RestaurantService {
 
   constructor(private readonly http: HttpClient) {}
 
-  public findAll(): Promise<Restaurant[]> {
-    return this.http.get<Restaurant[]>(`${environment.apiUrl}/restaurants`).toPromise()
+  public findAll(): Observable<Restaurant[]> {
+    return this.http.get<Restaurant[]>(`${environment.apiUrl}/restaurants`)
   }
 
   public findById(id: string): Promise<Restaurant> {
